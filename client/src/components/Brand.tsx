75388bd15ce7ad058dcfcb5ae8dd4a618bbe2447
Simon Balanoff <sjbalanoff@gmail.com>
2026-07-15 15:43:46 -0500

    Update branding

--- client/src/components/Brand.tsx
+++ client/src/components/Brand.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export function Brand({ compact = false }: { compact?: boolean }) {
     return (
         <Link className="brand" to="/">
-            <img src="/brand-mark.svg" alt="" />
+            <img src="/brand-mark.png" alt="" />
             <span>
                 <strong>Study Bank</strong>
                 {!compact ? <small>Colorado Gamma · CSU</small> : null}
